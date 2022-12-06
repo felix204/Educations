71a68955b926537cb192225456586b119127c241
felix204 <icigenaydincan@gmail.com>
2022-12-06 20:04:11 +0300

    components finish

---
+++ React/components/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/header';
+import './App.css';
+
+function App() {
+  return(
+    <div>
+      <h1>Hello</h1>
+      <Header />
+    </div>
+  )
+}
+
+export default App;
---
+++ React/components/src/components/header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div>
+            <h3>
+               Header bileşeni
+            </h3>
+        </div>
+    )
+}
+
+export default Header;
